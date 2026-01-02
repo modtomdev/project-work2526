df0cf50b3b61acd4bf94d2965390fabed7e88bc2
modtomdev <t.modolo.dev@gmail.com>
2026-01-02 12:19:00 +0100

    casing fix

--- frontend/dashboard/src/main.jsx
+++ frontend/dashboard/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import SvgSimulation from './Components/svgSimulation.jsx'
+import SvgSimulation from './Components/SvgSimulation.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
